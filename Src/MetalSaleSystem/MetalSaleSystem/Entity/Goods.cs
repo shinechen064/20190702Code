25cffc12e3fccf70feb55c1093af0eb12c94a698
SuperHT66 <76919744@qq.com>
2019-07-02 19:31:45 +0800

    贵金属销售系统:增加商品类

---
+++ Src/MetalSaleSystem/MetalSaleSystem/Entity/Goods.cs
@@ -0,0 +1,40 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace MetalSaleSystem.Entity
+{
+    public class Goods
+    {
+        private int goodsId;
+
+        public int GoodsId
+        {
+            get { return goodsId; }
+            set { goodsId = value; }
+        }
+        private string goodsName;
+
+        public string GoodsName
+        {
+            get { return goodsName; }
+            set { goodsName = value; }
+        }
+        private string unit;
+
+        public string Unit
+        {
+            get { return unit; }
+            set { unit = value; }
+        }
+        private string price;
+
+        public string Price
+        {
+            get { return price; }
+            set { price = value; }
+        }
+    }
+}
