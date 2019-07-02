5488bdba61ee2cd6ef5d96dd3aebc72c687d8aaa
SuperHT66 <76919744@qq.com>
2019-07-02 18:34:42 +0800

    贵金属销售系统：修改读取数据方式

---
+++ Src/MetalSaleSystem/MetalSaleSystem/Common/DataHelper.cs
@@ -0,0 +1,46 @@
+﻿using MetalSaleSystem.Entity;
+using System.Collections.Generic;
+
+namespace MetalSaleSystem.Common
+{
+    public class DataHelper
+    {
+        public List<MemberGradeEntity> GetMemberGradeData()
+        {
+            List<MemberGradeEntity> listMemberGrade = new List<MemberGradeEntity>();
+            MemberGradeEntity memberGrade1 = new MemberGradeEntity();
+            memberGrade1.GradeName = "普卡";
+            memberGrade1.Speed = 1;
+            memberGrade1.Start = 0;
+            memberGrade1.End = 10000;
+            memberGrade1.NextName = "金卡";
+            listMemberGrade.Add(memberGrade1);
+
+            MemberGradeEntity memberGrade2 = new MemberGradeEntity();
+            memberGrade2.GradeName = "金卡";
+            memberGrade2.Speed = 1;
+            memberGrade2.Start = 10000;
+            memberGrade2.End = 50000;
+            memberGrade2.NextName = "白金卡";
+            listMemberGrade.Add(memberGrade2);
+
+            MemberGradeEntity memberGrad3 = new MemberGradeEntity();
+            memberGrad3.GradeName = "白金卡";
+            memberGrad3.Speed = 1;
+            memberGrad3.Start = 50000;
+            memberGrad3.End = 100000;
+            memberGrad3.NextName = "钻石卡";
+            listMemberGrade.Add(memberGrad3);
+
+            MemberGradeEntity memberGrad4 = new MemberGradeEntity();
+            memberGrad4.GradeName = "钻石卡";
+            memberGrad4.Speed = 1;
+            memberGrad4.Start = 100000;
+            memberGrad4.End = 9999999;
+            memberGrad4.NextName = "";
+            listMemberGrade.Add(memberGrad4);
+
+            return listMemberGrade;
+        }
+    }
+}
---
+++ Src/MetalSaleSystem/MetalSaleSystem/Entity/MemberGradeEntity.cs
@@ -0,0 +1,112 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace MetalSaleSystem.Entity
+{
+    [Serializable]
+    public class MemberGradeEntity
+    {
+        private int gradeId;
+        private string gradeName;
+        private int start;
+        private int end;
+        private string nextName;
+        private double speed;
+
+        /// <summary>
+        /// 等级名称
+        /// </summary>
+        public string GradeName
+        {
+            get
+            {
+                return gradeName;
+            }
+
+            set
+            {
+                gradeName = value;
+            }
+        }
+        
+        /// <summary>
+        /// 积分倍速
+        /// </summary>
+        public double Speed
+        {
+            get
+            {
+                return speed;
+            }
+
+            set
+            {
+                speed = value;
+            }
+        }
+
+        /// <summary>
+        /// 等级开始区间
+        /// </summary>
+        public int Start
+        {
+            get
+            {
+                return start;
+            }
+
+            set
+            {
+                start = value;
+            }
+        }
+
+        /// <summary>
+        /// 等级结束区间
+        /// </summary>
+        public int End
+        {
+            get
+            {
+                return end;
+            }
+
+            set
+            {
+                end = value;
+            }
+        }
+
+        /// <summary>
+        /// 下一等级名称
+        /// </summary>
+        public string NextName
+        {
+            get
+            {
+                return nextName;
+            }
+
+            set
+            {
+                nextName = value;
+            }
+        }
+
+        public int GradeId
+        {
+            get
+            {
+                return gradeId;
+            }
+
+            set
+            {
+                gradeId = value;
+            }
+        }
+    }
+}
