3002e8d1f6357aaac7826ec373f8375e4f55c8d1
SuperHT66 <76919744@qq.com>
2019-07-02 21:43:23 +0800

    修改商品计算类

--- Src/MetalSaleSystem/MetalSaleSystem/Service/GoodsService.cs
+++ Src/MetalSaleSystem/MetalSaleSystem/Service/GoodsService.cs
@@ -9,24 +9,10 @@ namespace MetalSaleSystem.Service
 {
     public class GoodsService
     {
-        public List<Goods> listGoods;
-        public GoodsService()
+        private List<Goods> listGoods;
+        public GoodsService(List<Goods> listGoods)
         {
-            listGoods = new List<Goods>();
-            Goods goods001001 = new Goods(1, "世园会五十国钱币册", "册", 998.00, "001001", Discount.Discount1, OpenDoorRed.Full);
-            Goods goods001002 = new Goods(2, "2019北京世园会纪念银章大全40g", "盒", 1380.00, "001002", Discount.Discount9, OpenDoorRed.Full);
-            Goods goods003001 = new Goods(3, " 招财进宝", "条", 1580.00, "003001", Discount.Discount1, OpenDoorRed.Full);
-            Goods goods003002 = new Goods(4, "水晶之恋", "册", 980.00, "003002", Discount.Discount1, OpenDoorRed.Full3Half | OpenDoorRed.Full3Give1);
-            Goods goods002002 = new Goods(5, "中国经典钱币套装", "套", 998.00, "002002", Discount.Discount9 | Discount.Discount95, OpenDoorRed.Full);
-            Goods goods002001 = new Goods(6, "守扩之羽比翼双飞4.8g", "条", 1080.00, "002001", Discount.Discount95, OpenDoorRed.Full3Half | OpenDoorRed.Full3Give1);
-            Goods goods002003 = new Goods(7, "中国银象棋12g", "套", 698.00, "002003", Discount.Discount9, OpenDoorRed.Full3000 | OpenDoorRed.Full2000 | OpenDoorRed.Full1000);
-            listGoods.Add(goods001001);
-            listGoods.Add(goods001002);
-            listGoods.Add(goods003001);
-            listGoods.Add(goods003002);
-            listGoods.Add(goods002002);
-            listGoods.Add(goods002001);
-            listGoods.Add(goods002003);
+
         }
 
         /// <summary>
--- Src/MetalSaleSystem/MetalSaleSystem/Service/Result.cs
+++ Src/MetalSaleSystem/MetalSaleSystem/Service/Result.cs
@@ -15,6 +15,7 @@ namespace MetalSaleSystem.Service
         private StreamWriter m_swWriter;
         private OrderInformation m_objOrderInfo;
         private List<Member> m_objMember;
+        private List<Goods> listGoods ;
         public Result(OrderInformation objOI,string argOutputFile)
         {
             if(string.IsNullOrWhiteSpace(argOutputFile))
@@ -56,6 +57,23 @@ namespace MetalSaleSystem.Service
             m_objMember.Add(member);
             member = new Member(0, "张三", "9230009999", 198860, "");
             m_objMember.Add(member);
+
+            listGoods = new List<Goods>();
+            Goods goods = new Goods(1, "世园会五十国钱币册", "册", 998.00, "001001", Discount.Discount1, OpenDoorRed.Full);
+            listGoods.Add(goods);
+            goods = new Goods(2, "2019北京世园会纪念银章大全40g", "盒", 1380.00, "001002", Discount.Discount9, OpenDoorRed.Full);
+            listGoods.Add(goods);
+            goods = new Goods(3, " 招财进宝", "条", 1580.00, "003001", Discount.Discount1, OpenDoorRed.Full);
+            listGoods.Add(goods);
+            goods = new Goods(4, "水晶之恋", "册", 980.00, "003002", Discount.Discount1, OpenDoorRed.Full3Half | OpenDoorRed.Full3Give1);
+            listGoods.Add(goods);
+            goods = new Goods(5, "中国经典钱币套装", "套", 998.00, "002002", Discount.Discount9 | Discount.Discount95, OpenDoorRed.Full);
+            listGoods.Add(goods);
+            goods = new Goods(6, "守扩之羽比翼双飞4.8g", "条", 1080.00, "002001", Discount.Discount95, OpenDoorRed.Full3Half | OpenDoorRed.Full3Give1);
+            listGoods.Add(goods);
+            goods = new Goods(7, "中国银象棋12g", "套", 698.00, "002003", Discount.Discount9, OpenDoorRed.Full3000 | OpenDoorRed.Full2000 | OpenDoorRed.Full1000);
+            listGoods.Add(goods);
+
             try
             {
                 m_fsWriter = new FileStream(m_strOutputFile, FileMode.Create);
