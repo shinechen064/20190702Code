34e0dffa37effd2e402a56696e261ec448687da1
shinechen064 <714849400@qq.com>
2019-07-02 16:16:15 +0800

    1.提交命令行参数的实现 2.新增单元测试的模块

--- Src/MetalSaleSystem/MetalSaleSystem/Program.cs
+++ Src/MetalSaleSystem/MetalSaleSystem/Program.cs
@@ -1,5 +1,6 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.IO;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
@@ -10,7 +11,30 @@ namespace MetalSaleSystem
     {
         static void Main(string[] args)
         {
+            // 输入参数不合法
+            if (null == args || args.Length <= 1)
+            {
+                Console.WriteLine("please key something like:MetalSaleSystem.exe sample_command.json order_receipt.txt");
+                return;
+            }
+            string strInputFile = args[0];
+            string strOutputFile = args[1];
+            if (!File.Exists(strInputFile))
+            {
+                Console.WriteLine("input file {0} is not exist!", strInputFile);
+                return;
+            }
 
         }
+        public bool UnpackJsonData(string argFile)
+        {
+            if(!File.Exists(argFile))
+            {
+                Console.WriteLine("input file {0} is not exist!", argFile);
+                return false;
+            }
+            return false;
+        }
     }
+
 }
---
+++ Src/MetalSaleSystem/UnitTestMetalSaleSystem/UnitProgram.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+
+namespace UnitTestMetalSaleSystem
+{
+    [TestClass]
+    public class UnitProgram
+    {
+        [TestMethod]
+        public void TestUnpackJsonData()
+        {
+        }
+    }
+}
