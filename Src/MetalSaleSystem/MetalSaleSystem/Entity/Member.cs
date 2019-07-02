163f89717ab08c78bff5c1214e714be4355d18a2
shinechen064 <714849400@qq.com>
2019-07-02 18:37:33 +0800

    1.新增会员类 2.新会积分系统

---
+++ Src/MetalSaleSystem/MetalSaleSystem/Entity/Member.cs
@@ -0,0 +1,38 @@
+﻿using Newtonsoft.Json;
+using System.Collections.Generic;
+using System.Diagnostics;
+
+namespace MetalSaleSystem.Entity
+{
+    /// <summary>
+    /// Json帮助类
+    /// </summary>
+    public class Member
+    {
+        /// <summary>
+        /// 会员ID
+        /// </summary>
+        public string id;
+        /// <summary>
+        /// 会员姓名
+        /// </summary>
+        public string name;
+        /// <summary>
+        /// 会员等级
+        /// </summary>
+        public string grade;
+        /// <summary>
+        /// 会员卡号
+        /// </summary>
+        public string cardNo;
+        /// <summary>
+        ///会员积分
+        /// </summary>
+        public string jiFen;
+        /// <summary>
+        /// 会员优惠券
+        /// </summary>
+        public string discountCard;
+    }
+    
+}
---
+++ Src/MetalSaleSystem/MetalSaleSystem/Service/JiFenSystem.cs
@@ -0,0 +1,14 @@
+﻿using System.Collections.Generic;
+
+namespace MetalSaleSystem.Service
+{
+    /// <summary>
+    /// Json帮助类
+    /// </summary>
+    public class JiFenSystem
+    {
+        
+    }
+
+  
+}
