f4ee2bf7820e708c183224adae05eeef58bcbe02
SuperHT66 <76919744@qq.com>
2019-07-02 20:01:55 +0800

    贵金属销售系统：增加商品处理类

--- Src/MetalSaleSystem/MetalSaleSystem/Entity/Goods.cs
+++ Src/MetalSaleSystem/MetalSaleSystem/Entity/Goods.cs
@@ -10,6 +10,9 @@ namespace MetalSaleSystem.Entity
     {
         private int goodsId;
 
+        /// <summary>
+        /// ID
+        /// </summary>
         public int GoodsId
         {
             get { return goodsId; }
@@ -17,6 +20,9 @@ namespace MetalSaleSystem.Entity
         }
         private string goodsName;
 
+        /// <summary>
+        /// 商品名称
+        /// </summary>
         public string GoodsName
         {
             get { return goodsName; }
@@ -24,6 +30,9 @@ namespace MetalSaleSystem.Entity
         }
         private string unit;
 
+        /// <summary>
+        /// 单位
+        /// </summary>
         public string Unit
         {
             get { return unit; }
@@ -31,10 +40,58 @@ namespace MetalSaleSystem.Entity
         }
         private string price;
 
+        /// <summary>
+        /// 价格
+        /// </summary>
         public string Price
         {
             get { return price; }
             set { price = value; }
         }
+
+        private string batchNo;
+
+        /// <summary>
+        /// 编号
+        /// </summary>
+        public string BatchNo
+        {
+            get { return batchNo; }
+            set { batchNo = value; }
+        }
+
+        private Discount discount;
+
+        public Discount Discount
+        {
+            get { return discount; }
+            set { discount = value; }
+        }
+
+        private OpenDoorRed openDoorRed;
+
+        public OpenDoorRed OpenDoorRed
+        {
+            get { return openDoorRed; }
+            set { openDoorRed = value; }
+        }
+    }
+
+    /// <summary>
+    /// 打折卷
+    /// </summary>
+    public enum Discount
+    {
+        Discount95 = 95,
+        Discount9 = 9
+    }
+
+    public enum OpenDoorRed
+    {
+        Full3000 = "每满3000元减350",
+        Full2000 = "每满2000元减30",
+        Full1000 = "每满1000元减10",
+        Full3Half = "第3件半价",
+        Full3Give1 = "满3送1"
     }
 }
